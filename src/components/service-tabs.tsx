71382764ca9d6c69028e9c2e8ebf873b7e9a2f82
Meskat Rahman <bdzone010@gmail.com>
2025-07-18 13:03:37 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Primitive.button

--- src/components/service-tabs.tsx
+++ src/components/service-tabs.tsx
@@ -3,9 +3,8 @@ import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
 export function ServiceTabs() {
   return (
     <Tabs defaultValue="rides" className="flex w-full justify-center">
-      <TabsList className="grid w-full max-w-xs grid-cols-2">
+      <TabsList className="grid w-full max-w-xs grid-cols-1">
         <TabsTrigger value="rides">Rides</TabsTrigger>
-        <TabsTrigger value="eats">Eats</TabsTrigger>
       </TabsList>
     </Tabs>
   );
